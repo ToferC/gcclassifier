9fd18eefaf7efe2355f526993ab50f691ed6e5d1
toferc <cgeist7@gmail.com>
2017-06-01 09:08:29 -0400

    working ratings

--- classifier/forms.py
+++ classifier/forms.py
@@ -1,12 +1,12 @@
 from django import forms
-from django.forms import widgets
+from django.forms import widgets, BaseModelFormSet
 from django.forms.extras.widgets import SelectDateWidget
 from django.forms.widgets import CheckboxSelectMultiple
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
-from classifier.models import Document, Community, Tag
+from classifier.models import Document, Community, Tag, Rating, KeyWord, Relationship
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field, Fieldset
@@ -15,81 +15,110 @@ from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions, Inl
 # Forms
 
 class DocumentForm(forms.ModelForm): 
-	"""Form for users to add images"""
+    """Form for users to add images"""
 
-	class Meta:
-		model = Document
-		fields = "__all__"
-		exclude = ['creator', 'created_date', 'edited_date',
-		'slug']
+    class Meta:
+        model = Document
+        fields = "__all__"
+        exclude = ['creator', 'created_date', 'edited_date',
+        'slug']
 
-	def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-		super(DocumentForm, self).__init__(*args, **kwargs)
+        super(DocumentForm, self).__init__(*args, **kwargs)
 
-		self.helper = FormHelper(self)
-		self.helper.layout.append(
-			FormActions(
-				HTML("""<br><a committment="button" class="btn btn-default"
-					ref="classifier/document/{{ document.slug }}">Cancel</a> """),
-				Submit('save', 'Save'),))
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            FormActions(
+                HTML("""<br><a committment="button" class="btn btn-default"
+                    ref="classifier/document/{{ document.slug }}">Cancel</a> """),
+                Submit('save', 'Save'),))
 
-	def save(self, creator, commit=False):
-		instance = super(DocumentForm, self).save(commit=False)
-		instance.creator=creator
-		instance.save()
-		return instance
+    def save(self, creator, commit=False):
+        instance = super(DocumentForm, self).save(commit=False)
+        instance.creator=creator
+        instance.save()
+        return instance
 
 
 class CommunityForm(forms.ModelForm): 
-	"""Form for users to add images"""
+    """Form for users to add images"""
 
-	class Meta:
-		model = Community
-		fields = "__all__"
-		exclude = ['creator',
-		'created_date', 'edited_date', 'slug']
+    class Meta:
+        model = Community
+        fields = "__all__"
+        exclude = ['creator',
+        'created_date', 'edited_date', 'slug']
 
-	def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-		super(CommunityForm, self).__init__(*args, **kwargs)
+        super(CommunityForm, self).__init__(*args, **kwargs)
 
-		self.helper = FormHelper(self)
-		self.helper.layout.append(
-			FormActions(
-				HTML("""<br><a committment="button" class="btn btn-default"
-					ref="classifier/document/{{ document.slug }}">Cancel</a> """),
-				Submit('save', 'Save'),))
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            FormActions(
+                HTML("""<br><a committment="button" class="btn btn-default"
+                    ref="classifier/document/{{ document.slug }}">Cancel</a> """),
+                Submit('save', 'Save'),))
 
-	def save(self, creator, commit=False):
-		instance = super(CommunityForm, self).save(commit=False)
-		instance.creator=creator
-		instance.save()
-		return instance
+    def save(self, creator, commit=False):
+        instance = super(CommunityForm, self).save(commit=False)
+        instance.creator=creator
+        instance.save()
+        return instance
 
 
 class TagForm(forms.ModelForm): 
-	"""Form for users to add images"""
+    """Form for users to add images"""
 
-	class Meta:
-		model = Tag
-		fields = "__all__"
-		exclude = ['creator', 'user_generated',
-		'created_date', 'edited_date', 'slug']
+    class Meta:
+        model = Tag
+        fields = "__all__"
+        exclude = ['creator', 'user_generated',
+        'created_date', 'edited_date', 'slug']
 
-	def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
 
-		super(TagForm, self).__init__(*args, **kwargs)
+        super(TagForm, self).__init__(*args, **kwargs)
 
-		self.helper = FormHelper(self)
-		self.helper.layout.append(
-			FormActions(
-				HTML("""<br><a committment="button" class="btn btn-default"
-					ref="classifier/tag/{{ tag.slug }}">Cancel</a> """),
-				Submit('save', 'Save'),))
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            FormActions(
+                HTML("""<br><a committment="button" class="btn btn-default"
+                    ref="classifier/tag/{{ tag.slug }}">Cancel</a> """),
+                Submit('save', 'Save'),))
 
-	def save(self, creator, commit=False):
-		instance = super(TagForm, self).save(commit=False)
-		instance.creator=creator
-		instance.save()
-		return instance
+    def save(self, creator, commit=False):
+        instance = super(TagForm, self).save(commit=False)
+        instance.creator=creator
+        instance.save()
+        return instance
+
+
+class RatingForm(forms.ModelForm):
+    """Form for users to add images"""
+
+    class Meta:
+        model = Rating
+        fields = "__all__"
+        exclude = ["creator", "document", "user_generated",
+        "created_date", "edited_date"]
+
+    def __init__(self, *args, **kwargs):
+
+        super(RatingForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+        self.helper.layout.append(
+            FormActions(
+                HTML("""<br><a committment="button" class="btn btn-default"
+                    ref="classifier/">Cancel</a> """),
+                Submit('save', 'Save'),))
+
+    def save(self, creator, document, user_generated=False, commit=False):
+        instance = super(RatingForm, self).save(commit=False)
+        instance.creator=creator
+        instance.document=document
+        instance.user_generated=user_generated
+        instance.save()
+        return instance
--- classifier/views.py
+++ classifier/views.py
@@ -3,7 +3,11 @@ from classifier.models import Document, Community, Tag, Rating, Relationship, Ke
 from django.views.generic.edit import FormView, UpdateView, DeleteView, CreateView
 
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.forms import modelformset_factory
 
+from django.core import serializers
+
+from classifier.forms import RatingForm, DocumentForm, TagForm, RatingForm, CommunityForm
 
 from django.http import HttpResponseRedirect
 from classifier.forms import *
@@ -35,7 +39,7 @@ def document(request, document_slug):
      
         context_dict['document'] = document
 
-        context_dict['communities'] = Community.objects.filter(rating__document=document).distinct()
+        context_dict['ratings'] = Rating.objects.filter(document=document).distinct()
         #context_dict['tags'] = Tag.objects.filter(document=document).distinct()
         
     except Document.DoesNotExist:
@@ -117,18 +121,152 @@ def review_document(request, document_slug):
 
     try:
         document = Document.objects.get(slug=document_slug)
-        communities = Community.objects.all().distinct()
-
+        machine_ratings = serializers.serialize( "python",
+            Rating.objects.filter(document=document, user_generated=False),
+            fields=(
+                'atip_community',
+                'materiel_community',
+                'procurement_community',
+                'real_property_community',
+                'evaluator_community',
+                'communication_community',
+                'regulator_community',
+                'financial_community',
+                'im_community',
+                'it_community',
+                'auditor_community',
+                'security_community',
+                'hr_community',
+                'policy_community',
+                'science_community',
+                'service_community',)
+            )
+        user_ratings = serializers.serialize( "python",
+            Rating.objects.filter(document=document, user_generated=True),
+            fields=(
+                'atip_community',
+                'materiel_community',
+                'procurement_community',
+                'real_property_community',
+                'evaluator_community',
+                'communication_community',
+                'regulator_community',
+                'financial_community',
+                'im_community',
+                'it_community',
+                'auditor_community',
+                'security_community',
+                'hr_community',
+                'policy_community',
+                'science_community',
+                'service_community',)
+            )
+        
         context_dict['document'] = document
-        context_dict['communities'] = communities
+        context_dict['user_ratings'] = user_ratings
+        context_dict['machine_ratings'] = machine_ratings
 
+        if request.method == 'POST':
+
+            form = RatingForm(request.POST)
+
+            if form.is_valid():
+
+                form.save(creator=request.user, document=document,
+                    user_generated=True, commit=True)
+                form.save_m2m()
+
+                return HttpResponseRedirect("/classifier/document/{}".format(document.slug))
+
+
+            else:
+                print("Form validation error")
+
+        else:
+            form = RatingForm()
+            context_dict['form'] =  form
 
     except Document.DoesNotExist:
         pass
 
-    return render(request, 'classifier/review_document.html', context_dict)
+    return render(request, 'classifier/review_document.html',
+        context_dict)
 
 
+def document_results(request, document_slug):
+
+    context_dict = {}
+
+    try:
+        document = Document.objects.get(slug=document_slug)
+        machine_ratings = Rating.objects.filter(document=document).filter(user_generated=False)
+        user_ratings = Rating.objects.filter(document=document).filter(user_generated=True)
+
+        num_user_ratings = len(user_ratings)
+        num_machine_ratings = len(machine_ratings)
+
+        context_dict['num_user_ratings'] = num_user_ratings
+        context_dict['num_machine_ratings'] = num_machine_ratings
+
+        context_dict['atip_community'] = 0
+        context_dict['materiel_community'] = 0
+        context_dict['procurement_community'] = 0
+        context_dict['real_property_community'] = 0
+        context_dict['evaluator_community'] = 0
+        context_dict['communication_community'] = 0
+        context_dict['regulator_community'] = 0
+        context_dict['financial_community'] = 0
+        context_dict['im_community'] = 0
+        context_dict['it_community'] = 0
+        context_dict['auditor_community'] = 0
+        context_dict['security_community'] = 0
+        context_dict['hr_community'] = 0
+        context_dict['policy_community'] = 0
+        context_dict['science_community'] = 0
+        context_dict['service_community'] = 0
+
+        for rating in user_ratings:
+            context_dict['atip_community'] += rating.atip_community
+            context_dict['materiel_community'] += rating.materiel_community
+            context_dict['procurement_community'] += rating.procurement_community
+            context_dict['real_property_community'] += rating.real_property_community
+            context_dict['evaluator_community'] += rating.evaluator_community
+            context_dict['communication_community'] += rating.communication_community
+            context_dict['regulator_community'] += rating.regulator_community
+            context_dict['financial_community'] += rating.financial_community
+            context_dict['im_community'] += rating.im_community
+            context_dict['it_community'] += rating.it_community
+            context_dict['auditor_community'] += rating.auditor_community
+            context_dict['security_community'] += rating.security_community
+            context_dict['hr_community'] += rating.hr_community
+            context_dict['policy_community'] += rating.policy_community
+            context_dict['science_community'] += rating.science_community
+            context_dict['service_community'] += rating.service_community
+
+        context_dict['atip_community'] = context_dict['atip_community'] / len(user_ratings)
+        context_dict['materiel_community'] = context_dict['materiel_community'] / len(user_ratings)
+        context_dict['procurement_community'] = context_dict['procurement_community'] / len(user_ratings)
+        context_dict['real_property_community'] = context_dict['real_property_community'] / len(user_ratings)
+        context_dict['evaluator_community'] = context_dict['evaluator_community'] / len(user_ratings)
+        context_dict['communication_community'] = context_dict['communication_community'] / len(user_ratings)
+        context_dict['regulator_community'] = context_dict['regulator_community'] / len(user_ratings)
+        context_dict['financial_community'] = context_dict['financial_community'] / len(user_ratings)
+        context_dict['im_community'] = context_dict['im_community'] / len(user_ratings)
+        context_dict['it_community'] = context_dict['it_community'] / len(user_ratings)
+        context_dict['auditor_community'] = context_dict['auditor_community'] / len(user_ratings)
+        context_dict['security_community'] = context_dict['security_community'] / len(user_ratings)
+        context_dict['hr_community'] = context_dict['hr_community'] / len(user_ratings)
+        context_dict['policy_community'] = context_dict['policy_community'] / len(user_ratings)
+        context_dict['science_community'] = context_dict['science_community'] / len(user_ratings)
+        context_dict['service_community'] = context_dict['service_community'] / len(user_ratings)
+
+        context_dict['document'] = document
+
+    except Document.DoesNotExist:
+        pass
+
+    return render(request, 'classifier/review_document.html',
+        context_dict)
 
 @login_required
 def add_community(request):
